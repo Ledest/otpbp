6cd3ebfc82ba6878047c1da32bc94a24b91563c8
Led <ledest@gmail.com>
2015-08-28 16:16:49 +0300

    add gen_{event,fsm}:system_get_state/1 and gen_{event,fsm}:system_replace_state/2

--- src/otpbp_erl_compile.erl
+++ src/otpbp_erl_compile.erl
@@ -4,6 +4,6 @@
 -export([compile_cmdline/0]).
 -endif.
 
--ifndef(HAVE_epp__parse_file_2).
+-ifndef(HAVE_erl_compile__compile_cmdline_0).
 compile_cmdline() -> erl_compile:compile_cmdline(init:get_plain_arguments()).
 -endif.
---
+++ src/otpbp_gen_event.erl
@@ -0,0 +1,27 @@
+-module(otpbp_gen_event).
+
+-ifndef(HAVE_gen_event__system_get_state_1).
+-export([system_get_state/1]).
+-endif.
+-ifndef(HAVE_gen_event__system_replace_state_2).
+-export([system_replace_state/2]).
+-endif.
+
+-ifndef(HAVE_gen_event__system_get_state_1).
+system_get_state([_ServerName, MSL, _Hib]) ->
+    {ok, [{Mod, Id, State} || #handler{module = Mod, id = Id, state = State} <- MSL]}.
+-endif.
+
+-ifndef(HAVE_gen_event__system_replace_state_2).
+system_replace_state(StateFun, [ServerName, MSL, Hib]) ->
+    {NMSL, NStates} = lists:unzip([begin
+                                       Cur = {Mod, Id, State},
+                                       try
+                                           NState = {Mod, Id, NS} = StateFun(Cur),
+                                           {HS#handler{state = NS}, NState}
+                                       catch
+                                           _:_ -> {HS, Cur}
+                                       end
+                                   end || #handler{module = Mod, id = Id, state = State} = HS <- MSL]),
+    {ok, NStates, [ServerName, NMSL, Hib]}.
+-endif.
