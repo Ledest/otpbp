694e02896223cc609aadb308e8c923c7f0d15b48
Led <ledest@gmail.com>
2015-09-20 03:46:55 +0300

    otpbp_lists: remove filtermap/2

--- src/otpbp_lists.erl
+++ src/otpbp_lists.erl
@@ -3,14 +3,7 @@
 -ifndef(HAVE_lists__droplast_1).
 -export([droplast/1]).
 -endif.
--ifndef(HAVE_lists__filtermap_2).
--export([filtermap/2]).
--endif.
 
 -ifndef(HAVE_lists__droplast_1).
 droplast(L) -> lists:reverse(tl(lists:reverse(L))).
--endif.
-
--ifndef(HAVE_lists__filtermap_2).
-filtermap(F, L) -> lists:zf(F, L).
 -endif.
