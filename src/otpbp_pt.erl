2ecc1f9188e6d468a02f0aac5d0d6dad7c4fd471
Led <ledest@gmail.com>
2015-10-15 17:22:10 +0300

    add license

--- src/otpbp_pt.erl
+++ src/otpbp_pt.erl
@@ -1,3 +1,20 @@
+%%% Copyright 2015 Oleksandr Chumachenko <ledest@gmail.com>
+%%%
+%%% This file is part of OTPBP.
+%%%
+%%% OTPBP is free software: you can redistribute it and/or modify it
+%%% under the terms of the GNU General Public License as published by
+%%% the Free Software Foundation, either version 3 of the License, or
+%%% (at your option) any later version.
+%%%
+%%% OTPBP is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+%%% See the GNU General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU General Public License
+%%% along with OTPBP. If not, see <http://www.gnu.org/licenses/>.
+
 -module(otpbp_pt).
 -export([parse_transform/2]).
 
