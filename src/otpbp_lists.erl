fb3f9e5a5a045f95644356ef599bdb3efe7d555a
Led <ledest@gmail.com>
2016-04-29 16:48:03 +0300

    otpbp_lists: add join/2

--- src/otpbp_lists.erl
+++ src/otpbp_lists.erl
@@ -4,7 +4,19 @@
 -export([droplast/1]).
 -endif.
 
+-ifndef(HAVE_lists__join_2).
+-export([join/2]).
+-endif.
+
 -ifndef(HAVE_lists__droplast_1).
 droplast([_])  -> [];
 droplast([H|T]) -> [H|droplast(T)].
+-endif.
+
+-ifndef(HAVE_lists__join_2).
+join(_, []) -> [];
+join(Sep, [H|T]) -> [H|join_prepend(Sep, T)].
+
+join_prepend(_, []) -> [];
+join_prepend(Sep, [H|T]) -> [Sep, H|join_prepend(Sep, T)].
 -endif.
