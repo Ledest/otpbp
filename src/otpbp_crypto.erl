8bb2db00c52ebfe749c1ecba657db93715039c34
Led <ledest@gmail.com>
2016-05-11 01:28:33 +0300

    otpbp_crypto: add dss_sign/2, dss_sign/3 dss_verify/3, dss_verify/4, rsa_sign/2, rsa_sign/3, rsa_verify/3, rsa_verify/4, sign/4, verify/5; fix sha224_mac/2, sha256_mac/2, sha384_mac/2, sha512_mac/2; add sha224_mac/3, sha256_mac/3, sha384_mac/3, sha512_mac/3

--- src/otpbp_crypto.erl
+++ src/otpbp_crypto.erl
@@ -1,5 +1,36 @@
 -module(otpbp_crypto).
 
+-ifndef(HAVE_crypto__dss_sign_2).
+-export([dss_sign/2]).
+-endif.
+-ifndef(HAVE_crypto__dss_sign_3).
+-export([dss_sign/3]).
+-endif.
+-ifndef(HAVE_crypto__dss_verify_3).
+-export([dss_verify/3]).
+-endif.
+-ifndef(HAVE_crypto__dss_verify_4).
+-export([dss_verify/4]).
+-endif.
+-ifndef(HAVE_crypto__rsa_sign_2).
+-export([rsa_sign/2]).
+-endif.
+-ifndef(HAVE_crypto__rsa_sign_3).
+-export([rsa_sign/3]).
+-endif.
+-ifndef(HAVE_crypto__rsa_verify_3).
+-export([rsa_verify/3]).
+-endif.
+-ifndef(HAVE_crypto__rsa_verify_4).
+-export([rsa_verify/4]).
+-endif.
+-ifndef(HAVE_crypto__sign_4).
+-export([sign/4]).
+-endif.
+-ifndef(HAVE_crypto__verify_5).
+-export([verify/5]).
+-endif.
+
 -ifndef(HAVE_crypto__sha224_1).
 -export([sha224/1]).
 -endif.
@@ -29,14 +60,59 @@
 -ifndef(HAVE_crypto__sha224_mac_2).
 -export([sha224_mac/2]).
 -endif.
+-ifndef(HAVE_crypto__sha224_mac_3).
+-export([sha224_mac/3]).
+-endif.
 -ifndef(HAVE_crypto__sha256_mac_2).
 -export([sha256_mac/2]).
+-endif.
+-ifndef(HAVE_crypto__sha256_mac_3).
+-export([sha256_mac/3]).
 -endif.
 -ifndef(HAVE_crypto__sha384_mac_2).
 -export([sha384_mac/2]).
 -endif.
+-ifndef(HAVE_crypto__sha384_mac_3).
+-export([sha384_mac/3]).
+-endif.
 -ifndef(HAVE_crypto__sha512_mac_2).
 -export([sha512_mac/2]).
+-endif.
+-ifndef(HAVE_crypto__sha512_mac_3).
+-export([sha512_mac/3]).
+-endif.
+
+-ifndef(HAVE_crypto__dss_sign_2).
+dss_sign(Data, Key) -> crypto:sign(dss, sha, Data, Key).
+-endif.
+-ifndef(HAVE_crypto__dss_sign_3).
+dss_sign(Type, Data, Key) -> crypto:sign(dss, Type, Data, Key).
+-endif.
+-ifndef(HAVE_crypto__dss_verify_3).
+dss_verify(Data, Signature, Key) -> crypto:verify(dss, sha, Data, Signature, Key).
+-endif.
+-ifndef(HAVE_crypto__dss_verify_4).
+dss_verify(Type, Data, Signature, Key) -> crypto:verify(dss, Type, Data, Signature, Key).
+-endif.
+-ifndef(HAVE_crypto__rsa_sign_2).
+rsa_sign(Data, Key) -> crypto:sign(rsa, sha, Data, Key).
+-endif.
+-ifndef(HAVE_crypto__rsa_sign_3).
+rsa_sign(Type, Data, Key) -> crypto:sign(rsa, Type, Data, Key).
+-endif.
+-ifndef(HAVE_crypto__rsa_verify_3).
+rsa_verify(Data, Signature, Key) -> crypto:verify(rsa, sha, Data, Signature, Key).
+-endif.
+-ifndef(HAVE_crypto__rsa_verify_4).
+rsa_verify(Type, Data, Signature, Key) -> crypto:verify(rsa, Type, Data, Signature, Key).
+-endif.
+-ifndef(HAVE_crypto__sign_4).
+sign(dss, Type, Data, Key) -> crypto:dss_sign(Type, Data, Key);
+sign(rsa, Type, Data, Key) -> crypto:rsa_sign(Type, Data, Key).
+-endif.
+-ifndef(HAVE_crypto__verify_5).
+verify(dss, Type, Data, Signature, Key) -> crypto:dss_verify(Type, Data, Signature, Key);
+verify(rsa, Type, Data, Signature, Key) -> crypto:rsa_verify(Type, Data, Signature, Key).
 -endif.
 
 -ifndef(HAVE_crypto__sha224_1).
@@ -53,16 +129,28 @@ sha512(Data) -> crypto:hash(sha512, Data).
 -endif.
 
 -ifndef(HAVE_crypto__sha224_mac_2).
-sha224_mac(Key, Data) -> crypto:hash_mac(sha224, Key, Data).
+sha224_mac(Key, Data) -> crypto:hmac(sha224, Key, Data).
+-endif.
+-ifndef(HAVE_crypto__sha224_mac_3).
+sha224_mac(Key, Data, Size) -> crypto:hmac(sha224, Key, Data, Size).
 -endif.
 -ifndef(HAVE_crypto__sha256_mac_2).
-sha256_mac(Key, Data) -> crypto:hash_mac(sha256, Key, Data).
+sha256_mac(Key, Data) -> crypto:hmac(sha256, Key, Data).
+-endif.
+-ifndef(HAVE_crypto__sha256_mac_3).
+sha256_mac(Key, Data, Size) -> crypto:hmac(sha256, Key, Data, Size).
 -endif.
 -ifndef(HAVE_crypto__sha384_mac_2).
-sha384_mac(Key, Data) -> crypto:hash_mac(sha384, Key, Data).
+sha384_mac(Key, Data) -> crypto:hmac(sha384, Key, Data).
+-endif.
+-ifndef(HAVE_crypto__sha384_mac_3).
+sha384_mac(Key, Data, Size) -> crypto:hmac(sha384, Key, Data, Size).
 -endif.
 -ifndef(HAVE_crypto__sha512_mac_2).
-sha512_mac(Key, Data) -> crypto:hash_mac(sha512, Key, Data).
+sha512_mac(Key, Data) -> crypto:hmac(sha512, Key, Data).
+-endif.
+-ifndef(HAVE_crypto__sha512_mac_3).
+sha512_mac(Key, Data, Size) -> crypto:hmac(sha512, Key, Data, Size).
 -endif.
 
 -ifndef(HAVE_crypto__sha224_init_0).
