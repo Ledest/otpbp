79360068f3b06c34fad1a151312dcb47440066e3
Led <ledest@gmail.com>
2016-09-07 00:55:36 +0300

    otpbp_math: add ceil/1, floor/1

--- src/otpbp_math.erl
+++ src/otpbp_math.erl
@@ -4,6 +4,29 @@
 -export([log2/1]).
 -endif.
 
+-ifndef(HAVE_math__ceil_1).
+-export([ceil/1]).
+-endif.
+-ifndef(HAVE_math__floor_1).
+-export([floor/1]).
+-endif.
+
 -ifndef(HAVE_math__log2_1).
 log2(X) -> math:log(X) / math:log(2).
+-endif.
+
+-ifndef(HAVE_math__ceil_1).
+-ifndef(HAVE_erlang__ceil_1).
+ceil(X) -> float(erlang:ceil(X)).
+-else
+ceil(X) -> float(round(X + 0.5)).
+-endif.
+-endif.
+
+-ifndef(HAVE_math__floor_1).
+-ifndef(HAVE_erlang__floor_1).
+floor(X) -> float(erlang:floor(X)).
+-else
+floor(X) -> float(round(X - 0.5)).
+-endif.
 -endif.
