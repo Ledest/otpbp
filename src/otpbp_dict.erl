875987db7617360ffd1699d2ea5c3013757b6673
Led <ledest@gmail.com>
2016-12-20 01:05:31 +0200

    otpbp_dict: add take/2

--- src/otpbp_dict.erl
+++ src/otpbp_dict.erl
@@ -4,6 +4,18 @@
 -export([is_empty/1]).
 -endif.
 
+-ifndef(HAVE_dict__take_2).
+-export([take/2]).
+-endif.
+
 -ifndef(HAVE_dict__is_empty_1).
 is_empty(Dict) -> dict:size(Dict) =:= 0.
+-endif.
+
+-ifndef(HAVE_dict__take_2).
+take(Key, Dict) ->
+    case dict:find(Key, Dict) of
+        {ok, V} -> {V, dict:erase(Key, Dict)};
+        error -> error
+    end.
 -endif.
