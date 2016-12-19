9121b34eaa68e9929ad77806def7fcead0c6cca0
Led <ledest@gmail.com>
2016-12-20 01:05:55 +0200

    otpbp_orddict: add take/2

--- src/otpbp_orddict.erl
+++ src/otpbp_orddict.erl
@@ -4,6 +4,19 @@
 -export([is_empty/1]).
 -endif.
 
+-ifndef(HAVE_orddict__take_2).
+-export([take/2]).
+-endif.
+
 -ifndef(HAVE_orddict__is_empty_1).
 is_empty(Dict) -> orddict:size(Dict) =:= 0.
+-endif.
+
+-ifndef(HAVE_orddict__take_2).
+take(Key, Dict) -> take(Key, Dict, []).
+
+take(Key, [{K, _}|_], _Acc) when Key < K -> error;
+take(Key, [{K, _} = P|D], Acc) when Key > K -> take(Key, D, [P|Acc]);
+take(_Key, [{_K, Value}|D], Acc) -> {Value, lists:reverse(Acc, D)};
+take(_, [], _) -> error.
 -endif.
