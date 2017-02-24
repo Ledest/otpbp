5c5f0ff2ed1e01df745ecd11d29878159d25fb53
Led <ledest@gmail.com>
2017-02-24 23:42:51 +0200

    add otpbp_filename module

---
+++ src/otpbp_filename.erl
@@ -0,0 +1,23 @@
+-module(otpbp_filename).
+
+-ifndef(HAVE_safe_relative_path_1).
+-export([safe_relative_path/1]).
+-endif.
+
+-ifndef(HAVE_safe_relative_path_1).
+safe_relative_path(Path) ->
+    case filename:pathtype(Path) of
+        relative -> safe_relative_path(filename:split(Path), []);
+        _ -> unsafe
+    end.
+
+safe_relative_path([$.|T], Acc) -> safe_relative_path(T, Acc);
+safe_relative_path([<<$.>>|T], Acc) -> safe_relative_path(T, Acc);
+safe_relative_path([".."|_], []) -> unsafe;
+safe_relative_path([<<"..">>|_], []) -> unsafe;
+safe_relative_path([".."|T], [_|Acc]) -> safe_relative_path(T, Acc);
+safe_relative_path([<<"..">>|T], [_|Acc]) -> safe_relative_path(T, Acc);
+safe_relative_path([H|T], Acc) -> safe_relative_path(T, [H|Acc]);
+safe_relative_path([], []) -> [];
+safe_relative_path([], Acc) -> filename:join(lists:reverse(Acc)).
+-endif.
