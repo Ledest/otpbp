006d2f4a5f9dd8ee83fdb6a7feff98cf27b67e98
Led <ledest@gmail.com>
2017-04-11 14:09:27 +0300

    otpbp_ets: add update_counter/4; update take/2

--- src/otpbp_ets.erl
+++ src/otpbp_ets.erl
@@ -1,12 +1,25 @@
 -module(otpbp_ets).
 
+-ifndef(HAVE_ets__update_counter_4).
+-export([update_counter/4]).
+-endif.
+
 -ifndef(HAVE_ets__take_2).
 -export([take/2]).
 -endif.
 
+-ifndef(HAVE_ets__update_counter_4).
+update_counter(Tab, Key, Incr, Default) when is_tuple(Default) ->
+    ets:insert_new(Tab, Default),
+    ets:update_counter(Tab, Key, Incr).
+-endif.
+
 -ifndef(HAVE_ets__take_2).
 take(Tab, Key) ->
-    Objects = ets:lookup(Tab, Key),
-    Objects =:= [] orelse ets:delete(Tab, Key),
-    Objects.
+    case ets:lookup(Tab, Key) of
+        [] -> false;
+        Objects ->
+            ets:delete(Tab, Key),
+            Objects
+    end.
 -endif.
