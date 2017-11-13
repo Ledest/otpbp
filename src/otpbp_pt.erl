a89e00f7dad3824f28e07dac39567a9f397d6ca2
Led <ledest@gmail.com>
2017-11-13 02:13:40 +0200

    otpbp_pt: add uri_string:is_host/1, uri_string:is_path/1

--- src/otpbp_pt.erl
+++ src/otpbp_pt.erl
@@ -118,7 +118,8 @@
                               {{string, [find, slice, split], [2, 3]}, otpbp_string},
                               {{string, replace, [3, 4]}, otpbp_string},
                               {{string, trim, [1, 2, 3]}, otpbp_string},
-                              {{supervisor, get_childspec, 2}, otpbp_supervisor}]).
+                              {{supervisor, get_childspec, 2}, otpbp_supervisor},
+                              {{uri_string, [is_host, is_path], 2}, otpbp_uri_string}]).
 
 -ifdef(buggy__revert_implicit_fun_1a).
 -ifndef(buggy__revert_implicit_fun).
