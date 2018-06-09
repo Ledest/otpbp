75f51576119c98ab49d2e112174767d06fcf920f
Led <ledest@gmail.com>
2018-06-09 23:58:35 +0300

    otpbp_lib: remove eval_str/1 (in erl_eval from Erlang/OTP 21)

--- src/otpbp_lib.erl
+++ src/otpbp_lib.erl
@@ -18,9 +18,6 @@
 -ifndef(HAVE_lib__progname_0).
 -export([progname/0]).
 -endif.
--ifndef(HAVE_lib__eval_str_1).
--export([eval_str/1]).
--endif.
 
 -ifndef(HAVE_lib__nonl_1).
 nonl([$\n|T]) -> nonl(T);
@@ -58,24 +55,4 @@ progname() ->
         {ok, [[Prog]]} -> list_to_atom(Prog);
         _ -> no_prog_name
     end.
--endif.
-
--ifndef(HAVE_lib__eval_str_1).
-eval_str(Str) when is_list(Str) ->
-    case erl_scan:tokens([], Str, 0) of
-        {more, _} -> {error, "Incomplete form (missing .<cr>)??"};
-        {done, {ok, Toks, _}, Rest} ->
-            case lists:all(fun(C) -> C =:= $\s orelse C =:= $\n orelse C =:= $\t end, Rest) andalso
-                 erl_parse:parse_exprs(Toks) of
-                {ok, Exprs} -> case catch erl_eval:exprs(Exprs, erl_eval:new_bindings()) of
-                                   {value, Val, _} -> {ok, Val};
-                                   Other -> {error, result("*** eval: ~p", [Other])}
-                               end;
-                {error, {_Line, Mod, Args}} -> {error, result("*** ~ts", [Mod:format_error(Args)])};
-                false -> {error, result("Non-white space found after end-of-form :~ts", [Rest])}
-            end
-    end;
-eval_str(Bin) when is_binary(Bin) -> eval_str(binary_to_list(Bin)).
-
-result(F, D) -> lists:flatten(io_lib:format(F, D)).
 -endif.
