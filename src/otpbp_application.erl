7272d197b37bae60c000f207d363841a83dae52e
Led <ledest@gmail.com>
2019-10-05 01:48:07 +0300

    add application:set_env/1, application:set_env/2

---
+++ src/otpbp_application.erl
@@ -0,0 +1,38 @@
+-module(otpbp_application).
+
+-ifndef(HAVE_application__set_env_2).
+-ifdef(HAVE_application_controller__set_env_2).
+-export([set_env/2]).
+-ifndef(NEED_application__set_env_2).
+-define(NEED_application__set_env_2, true).
+-endif.
+-endif.
+-endif.
+
+-ifndef(HAVE_application__set_env_1).
+-ifdef(HAVE_application__set_env_2).
+-export([set_env/1]).
+-import(application, [set_env/2]).
+-else.
+-ifdef(HAVE_application_controller__set_env_2).
+-export([set_env/1]).
+-ifndef(NEED_application__set_env_2).
+-define(NEED_application__set_env_2, true).
+-endif.
+-endif.
+-endif.
+-endif.
+
+-ifdef(NEED_application__set_env_2).
+set_env(Config, Opts) when is_list(Config), is_list(Opts) ->
+    case application_controller:set_env(Config, Opts) of
+        ok -> ok;
+        {error, Msg} -> error({badarg, Msg}, [Config, Opts])
+    end.
+-endif.
+
+-ifndef(HAVE_application__set_env_1).
+-ifdef(HAVE_application_controller__set_env_2).
+set_env(Config) when is_list(Config) -> set_env(Config, []).
+-endif.
+-endif.
