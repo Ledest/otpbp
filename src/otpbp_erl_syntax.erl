83bf3f9874e0115ca0f9cf21fb2cfe3aa0f60456
Led <ledest@gmail.com>
2019-10-27 00:04:09 +0300

    otpbp_erl_syntax: remove char_literal/2, string_literal/2

--- src/otpbp_erl_syntax.erl
+++ src/otpbp_erl_syntax.erl
@@ -1,21 +1,7 @@
 -module(otpbp_erl_syntax).
 
--ifndef(HAVE_erl_syntax__char_literal_2).
--export([char_literal/2]).
--endif.
--ifndef(HAVE_erl_syntax__string_literal_2).
--export([string_literal/2]).
--endif.
 -ifndef(HAVE_erl_syntax__record_access_2).
 -export([record_access/2]).
--endif.
-
--ifndef(HAVE_erl_syntax__char_literal_2).
-char_literal(Node, latin1) -> erl_syntax:char_literal(Node).
--endif.
-
--ifndef(HAVE_erl_syntax__string_literal_2).
-string_literal(Node, latin1) -> erl_syntax:string_literal(Node).
 -endif.
 
 -ifndef(HAVE_erl_syntax__record_access_2).
