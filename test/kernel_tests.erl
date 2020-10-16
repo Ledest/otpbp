511ef4d1708bbaf5509b0feebbf5ae08c96c1eb8
Led <ledest@gmail.com>
2020-10-17 00:40:41 +0300

    kernel_tests: add os_test/0

--- test/kernel_tests.erl
+++ test/kernel_tests.erl
@@ -8,3 +8,10 @@ code_test() ->
     ?assertEqual(code:module_status(?MODULE), loaded),
     ?assertEqual(code:module_status('TEST'), not_loaded),
     ?assert(is_list(code:modified_modules())).
+
+
+os_test() ->
+    Env = os:env(),
+    ?assertEqual(Env, os:list_env_vars()),
+    ?assertEqual(lists:map(fun({N, V}) -> N ++ [$=|V] end, Env), os:getenv()),
+    ok.
