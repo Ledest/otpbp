3b635c4c66916e9076016737128fdcc0c8c125ae
Led <ledest@gmail.com>
2020-12-11 23:36:32 +0200

    otpbp_erts_internal: add map_next/3

---
+++ src/otpbp_erts_internal.erl
@@ -0,0 +1,12 @@
+-module(otpbp_erts_internal).
+
+-ifndef(HAVE_etrs_internal__map_next_3).
+% OTP 21.0
+-export([map_next/3]).
+-endif.
+
+-ifndef(HAVE_etrs_internal__map_next_3).
+% OTP 21.0
+map_next(0, M, iterator) when is_map(M) -> lists:foldr(fun({K, V}, A) -> {K, V, A} end, none, maps:to_list(M));
+map_next(0, M, L) when is_list(L) -> lists:reverse(maps:fold(fun(K, V, A) -> [{K, V}|A] end, [], M), L).
+-endif.
--- src/otpbp_pt.erl
+++ src/otpbp_pt.erl
@@ -56,6 +56,7 @@
                               {{erl_syntax, record_access, 2}, otpbp_erl_syntax},
                               {{error_logger, get_format_depth, 0}, otpbp_error_logger},
                               {{error_logger, limit_term, 1}, otpbp_error_logger},
+                              {{erts_internal, map_next, 3}, otpbp_erts_internal},
                               {{ets, take, 2}, otpbp_ets},
                               {{ets, update_counter, 4}, otpbp_ets},
                               {{file, del_dir_r, 1}, otpbp_file},
