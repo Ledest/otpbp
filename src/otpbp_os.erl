2cc1d5e079739146d82df2a67b0b3b2bd69ee58f
Led <ledest@gmail.com>
2021-02-07 22:59:35 +0200

    otpbp_os: remove system_time/1, getenv/2

--- src/otpbp_os.erl
+++ src/otpbp_os.erl
@@ -1,13 +1,5 @@
 -module(otpbp_os).
 
--ifndef(HAVE_os__system_time_1).
-% OTP 18.0
--export([system_time/1]).
--endif.
--ifndef(HAVE_os__getenv_2).
-% OTP 18.0
--export([getenv/2]).
--endif.
 -ifndef(HAVE_os__cmd_2).
 % OTP 20.2.3
 -export([cmd/2]).
@@ -19,29 +11,6 @@
 -ifndef(HAVE_os__list_env_vars_0).
 % OTP 21.0 - 23.x
 -export([list_env_vars/0]).
--endif.
-
--ifndef(HAVE_os__system_time_1).
-system_time(seconds) ->
-    {MegaSeconds, Seconds, _} = os:timestamp(),
-    MegaSeconds * 1000000 + Seconds;
-system_time(micro_seconds) ->
-    {MegaSeconds, Seconds, MicroSeconds} = os:timestamp(),
-    MegaSeconds * 1000000 * 1000000 + Seconds * 1000000 + MicroSeconds;
-system_time(milli_seconds) ->
-    {MegaSeconds, Seconds, MicroSeconds} = os:timestamp(),
-    MegaSeconds * 1000000 * 1000 + Seconds * 1000 + round(MicroSeconds / 1000);
-system_time(nano_seconds) ->
-    {MegaSeconds, Seconds, MicroSeconds} = os:timestamp(),
-    MegaSeconds * 1000000 * 1000000 * 1000 + Seconds * 1000000 * 1000 + MicroSeconds * 1000.
--endif.
-
--ifndef(HAVE_os__getenv_2).
-getenv(VarName, DefaultValue) ->
-    case os:getenv(VarName) of
-        false -> DefaultValue;
-        Value -> Value
-    end.
 -endif.
 
 -ifndef(HAVE_os__cmd_2).
--- src/otpbp_pt.erl
+++ src/otpbp_pt.erl
@@ -76,9 +76,8 @@
                               {{math, [ceil, floor, log2], 1}, otpbp_math},
                               {{orddict, take, 2}, otpbp_orddict},
                               {{ordsets, is_empty, 1}, otpbp_ordsets},
-                              {{os, [cmd, getenv], 2}, otpbp_os},
+                              {{os, cmd, 2}, otpbp_os},
                               {{os, [env, list_env_vars], 0}, otpbp_os},
-                              {{os, system_time, 1}, otpbp_os},
                               {{proplists, from_map, 1}, {maps, to_list}}, % OTP 24.0
                               {{proplists, to_map, [1, 2]}, otpbp_proplists},
                               {{queue, [all, any, delete, delete_r, delete_with, delete_with_r, filtermap, foreach], 2},
