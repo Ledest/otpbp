a5c6057b6c346b2404838a7edaf221c995be10b7
Led <ledest@gmail.com>
2021-02-13 20:55:26 +0200

    otpbp_crypto: add mac/3, mac/4

--- src/otpbp_crypto.erl
+++ src/otpbp_crypto.erl
@@ -87,6 +87,19 @@
 -export([supports/1]).
 -endif.
 
+-ifndef(HAVE_crypto__mac_3).
+% OTP 22.1
+-export([mac/3]).
+-endif.
+-ifndef(HAVE_crypto__mac_4).
+% OTP 22.1
+-export([mac/4]).
+-endif.
+
+-ifdef(HAVE_crypto__mac_4).
+-import(crypto, [mac/4]).
+-endif.
+
 -ifndef(HAVE_crypto__dss_sign_2).
 dss_sign(Data, Key) -> crypto:sign(dss, sha, Data, Key).
 -endif.
@@ -169,4 +182,93 @@ supports(T) when T =:= hashs; T =:= ciphers; T =:= public_keys; T =:= macs; T =:
         {_, Support} -> Support;
         _ -> []
     end.
+-endif.
+
+-ifndef(HAVE_crypto__mac_3).
+mac(Type, Key, Data) -> mac(Type, undefined, Key, Data).
+-endif.
+
+-ifndef(HAVE_crypto__mac_4).
+-ifdef(HAVE_crypto__poly1305_2).
+mac(poly1305, T, Key, Data) ->
+    try
+        crypto:poly1305(Key, Data)
+    catch
+        error:badarg ->
+            error(case is_binary(Data) orelse is_list(Data) andalso lists:all(fun is_iodata/1, Data) of
+                      true ->
+                          case is_binary(Key) orelse is_list(Key) andalso lists:all(fun is_iodata/1, Key) of
+                              true -> {badarg,{"mac.c",231},"Bad key length"};
+                              _false -> {badarg, {"mac.c", 216}, "Bad key"}
+                          end;
+                      _false -> {badarg, {"mac.c", 179}, "Bad text"}
+                  end,
+                  [poly1305, T, Key, Data]);
+        C:R -> erlang:C(R, [poly1305, T, Key, Data])
+    end;
+mac(hmac, T, Key, Data) -> hmac(T, Key, Data);
+mac(cmac, T, Key, Data) -> cmac(T, Key, Data);
+mac(T, SubType, Key, Data) -> mac_unknown(T, SubType, Key, Data).
+-else.
+mac(hmac, T, Key, Data) -> hmac(T, Key, Data);
+mac(cmac, T, Key, Data) -> cmac(T, Key, Data);
+mac(T, SubType, Key, Data) -> mac_unknown(T, SubType, Key, Data).
+-endif.
+is_iodata(E) ->
+    is_binary(E) orelse try iolist_size(E) of
+                            _ -> true
+                        catch
+                            _:_ -> false
+                        end.
+
+hmac(T, Key, Data) ->
+    try
+        crypto:hmac(T, Key, Data)
+    catch
+        error:badarg ->
+            error(case is_iodata(Data) of
+                      true ->
+                          case is_iodata(Key) of
+                              true -> {badarg, {"mac.c", 259}, "Bad digest algorithm for HMAC"};
+                              _false -> {badarg, {"mac.c", 216}, "Bad key"}
+                          end;
+                      _false -> {badarg, {"mac.c", 179}, "Bad text"}
+                  end,
+                  [hmac, T, Key, Data]);
+        C:R -> erlang:C(R, [hmac, T, Key, Data])
+    end.
+
+-ifdef(HAVE_crypto__cmac_3).
+cmac(T, Key, Data) ->
+    try
+        crypto:cmac(T, Key, Data)
+    catch
+        error:badarg ->
+            error(case is_iodata(Data) of
+                      true ->
+                          case is_iodata(Key) of
+                              true ->
+                                  case lists:member(T, ciphers()) of
+                                      true -> {badarg, {"mac.c", 306}, "Bad key size"};
+                                      _false -> {badarg, {"mac.c", 303}, "Unknown cipher"}
+                                  end;
+                              _false -> {badarg, {"mac.c", 216}, "Bad key"}
+                          end;
+                      _false -> {badarg, {"mac.c", 179}, "Bad text"}
+                  end,
+                  [cmac, T, Key, Data]);
+        C:R -> erlang:C(R, [cmac, T, Key, Data])
+    end.
+
+-compile({inline, ciphers/0}).
+ciphers() ->
+    {_, Ciphers} = lists:keyfind(ciphers, 1, crypto:supports()),
+    Ciphers.
+-else.
+cmac(T, Key, Data) -> mac_unknown(cmac, T, Key, Data).
+-endif.
+
+mac_unknown(T, SubType, Key, Data) -> error({badarg, {"mac.c", 229}, "Unknown mac algorithm"}, [T, SubType, Key, Data]).
+
+-compile({inline, [hmac/3, cmac/3, mac_unknown/4]}).
 -endif.
