aca8cb0641c769e564dd00eb4eb740cfa4a8bed7
Led <ledest@gmail.com>
2023-03-23 01:20:53 +0200

    otpbp_erlang: add iolist_to_iovec/1, term_to_iovec/1,2

--- src/otpbp_erlang.erl
+++ src/otpbp_erlang.erl
@@ -8,6 +8,10 @@
 % OTP 20.0
 -export([floor/1]).
 -endif.
+-ifndef(HAVE_erlang__iolist_to_iovec_1).
+% OTP 20.1
+-export([iolist_to_iovec/1]).
+-endif.
 -ifndef(HAVE_erlang__atom_to_binary_1).
 % OTP 23.0
 -export([atom_to_binary/1]).
@@ -19,6 +23,14 @@
 -ifndef(HAVE_erlang__binary_to_existing_atom_1).
 % OTP 23.0
 -export([binary_to_existing_atom/1]).
+-endif.
+-ifndef(HAVE_erlang__term_to_iovec_1).
+% OTP 23.0
+-export([term_to_iovec/1]).
+-endif.
+-ifndef(HAVE_erlang__term_to_iovec_2).
+% OTP 23.0
+-export([term_to_iovec/2]).
 -endif.
 -ifndef(HAVE_erlang__get_stacktrace_0).
 % OTP 24.0
@@ -49,6 +61,11 @@ floor(X) -> round(X - 0.5).
 -endif.
 -endif.
 
+-ifndef(HAVE_erlang__iolist_to_iovec_1).
+iolist_to_iovec(B) when is_binary(B) -> [B];
+iolist_to_iovec(L) -> [list_to_binary(L)].
+-endif.
+
 -ifndef(HAVE_erlang__atom_to_binary_1).
 atom_to_binary(A) -> atom_to_binary(A, utf8).
 -endif.
@@ -57,6 +74,13 @@ binary_to_atom(B) -> binary_to_atom(B, utf8).
 -endif.
 -ifndef(HAVE_erlang__binary_to_existing_atom_1).
 binary_to_existing_atom(B) -> binary_to_existing_atom(B, utf8).
+-endif.
+
+-ifndef(HAVE_erlang__term_to_iovec_1).
+term_to_iovec(T) -> [term_to_binary(T)].
+-endif.
+-ifndef(HAVE_erlang__term_to_iovec_2).
+term_to_iovec(T, O) -> [term_to_binary(T, O)].
 -endif.
 
 -ifndef(HAVE_erlang__get_stacktrace_0).
