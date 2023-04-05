2864f62f94682eb9fd4c1a09419f70200da4ba6f
Led <ledest@gmail.com>
2023-04-05 17:38:27 +0300

    Add otpbp_proc_lib
    - start_monitor/3,4,5

---
+++ src/otpbp_proc_lib.erl
@@ -0,0 +1,86 @@
+-module(otpbp_proc_lib).
+
+-ifndef(HAVE_proc_lib__start_monitor_3).
+% OTP 23.0
+-export([start_monitor/3]).
+-endif.
+-ifndef(HAVE_proc_lib__start_monitor_4).
+% OTP 23.0
+-export([start_monitor/4]).
+-endif.
+-ifndef(HAVE_proc_lib__start_monitor_5).
+% OTP 23.0
+-export([start_monitor/5]).
+-endif.
+
+-ifndef(HAVE_proc_lib__start_monitor_3).
+start_monitor(M, F, A) when is_atom(M), is_atom(F), is_list(A) -> start_monitor(M, F, A, infinity).
+-endif.
+
+-ifndef(HAVE_proc_lib__start_monitor_4).
+start_monitor(M, F, A, Timeout) when is_atom(M), is_atom(F), is_list(A) ->
+    sync_start_monitor(spawn_mon(M, F, A), Timeout).
+
+-compile({inline, [spawn_mon/3, get_my_name/0, proc_info/2, check/1, get_ancestors/0]}).
+
+spawn_mon(M, F, A) -> spawn_monitor(proc_lib, init_p, [get_my_name(), get_ancestors(), M, F, A]).
+
+get_my_name() ->
+    case proc_info(self(), registered_name) of
+        {registered_name, Name} -> Name;
+        _ -> self()
+    end.
+
+proc_info(Pid, Item) when node(Pid) =:= node() -> process_info(Pid, Item);
+proc_info(Pid, Item) ->
+    case lists:member(node(Pid), nodes()) of
+        true -> check(rpc:call(node(Pid), erlang, process_info, [Pid, Item]));
+        _ -> hidden
+    end.
+
+check({badrpc, nodedown}) -> undefined;
+check({badrpc, Error}) -> Error;
+check(Res) -> Res.
+
+get_ancestors() ->
+    case get('$ancestors') of
+        A when is_list(A) -> A;
+        _ -> []
+    end.
+
+-ifndef(NEED_sync_start_monitor_2).
+-define(NEED_sync_start_monitor_2, true).
+-endif.
+-endif.
+
+-ifndef(HAVE_proc_lib__start_monitor_5).
+start_monitor(M, F, A, Timeout, SpawnOpts) when is_atom(M), is_atom(F), is_list(A) ->
+    lists:member(monitor, SpawnOpts) andalso error(badarg, [M, F, A, Timeout, SpawnOpts]),
+    sync_start_monitor(proc_lib:spawn_opt(M, F, A, [monitor|SpawnOpts]), Timeout).
+
+-ifndef(NEED_sync_start_monitor_2).
+-define(NEED_sync_start_monitor_2, true).
+-endif.
+-endif.
+
+-ifdef(NEED_sync_start_monitor_2).
+sync_start_monitor({Pid, Ref}, Timeout) ->
+    receive
+        {ack, Pid, Return} -> {Return, Ref};
+        {'DOWN', Ref, process, Pid, Reason} = Down ->
+            self() ! Down,
+            {{error, Reason}, Ref}
+    after Timeout ->
+            kill_flush(Pid),
+            {{error, timeout}, Ref}
+    end.
+
+-compile({inline, [kill_flush/1]}).
+kill_flush(Pid) ->
+    unlink(Pid),
+    exit(Pid, kill),
+    receive
+        {'EXIT', Pid, _} -> ok
+    after 0 -> ok
+    end.
+-endif.
