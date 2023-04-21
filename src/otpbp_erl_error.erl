7e72e270302e5687c877106b128e73653b55f1cb
Led <ledest@gmail.com>
2023-04-21 08:00:13 +0300

    otpbp_erl_error: update format_exception/3

--- src/otpbp_erl_error.erl
+++ src/otpbp_erl_error.erl
@@ -8,20 +8,22 @@
 % OTP 24.0
 -export([format_exception/3]).
 -endif.
-
 -ifndef(HAVE_erl_error__format_exception_4).
 % OTP 24.0
 -export([format_exception/4]).
 -endif.
 
 -ifndef(HAVE_erl_error__format_exception_3).
+-ifdef(HAVE_erl_error__format_exception_4).
+format_exception(Class, Reason, StackTrace) -> erl_error:format_exception(Class, Reason, StackTrace, #{}).
+-else.
 format_exception(Class, Reason, StackTrace) ->
     erl_error:format_exception(1, Class, Reason, StackTrace,
                                fun(_, _, _) -> false end,
                                fun(Term, I) -> io_lib:print(Term, I, 80, 30) end,
                                unicode).
 -endif.
-
+-endif.
 
 -ifndef(HAVE_erl_error__format_exception_4).
 format_exception(Class, Reason, StackTrace, Options) ->
