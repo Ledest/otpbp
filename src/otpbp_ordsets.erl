ae535d5ce48d2953d87a1a5f230e3f4493a43d85
Led <ledest@gmail.com>
2023-06-07 15:34:15 +0300

    otpbp_ordsets: add map2, filtermap/2

--- src/otpbp_ordsets.erl
+++ src/otpbp_ordsets.erl
@@ -4,7 +4,23 @@
 % OTP 21.0
 -export([is_empty/1]).
 -endif.
+-ifndef(HAVE_ordsets__map_2).
+% OTP 26.1
+-export([map/2]).
+-endif.
+-ifndef(HAVE_ordsets__filtermap_2).
+% OTP 26.1
+-export([filtermap/2]).
+-endif.
 
 -ifndef(HAVE_ordsets__is_empty_1).
 is_empty(S) -> S =:= [].
+-endif.
+
+-ifndef(HAVE_ordsets__map_2).
+map(F, S) -> ordsets:from_list(lists:map(F, S)).
+-endif.
+
+-ifndef(HAVE_ordsets__filtermap_2).
+filtermap(F, S) -> ordsets:from_list(lists:filtermap(F, S)).
 -endif.
