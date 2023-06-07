179b2c52660d239d7a68aba914b9485968d75ec6
Led <ledest@gmail.com>
2023-06-07 15:33:54 +0300

    otpbp_sets: add map2, filtermap/2

--- src/otpbp_sets.erl
+++ src/otpbp_sets.erl
@@ -12,6 +12,14 @@
 % OTP 24.0
 -export([new/1]).
 -endif.
+-ifndef(HAVE_sets__map_2).
+% OTP 26.1
+-export([map/2]).
+-endif.
+-ifndef(HAVE_sets__filtermap_2).
+% OTP 26.1
+-export([filtermap/2]).
+-endif.
 
 -ifndef(HAVE_sets__is_empty_1).
 is_empty(S) -> sets:size(S) =:= 0.
@@ -23,4 +31,22 @@ from_list(List, [{version, 1}]) -> sets:from_list(List).
 
 -ifndef(HAVE_sets__new_1).
 new([{version, 1}]) -> sets:new().
+-endif.
+
+-ifndef(HAVE_sets__map_2).
+map(F, S) when is_function(F, 1) ->
+    sets:is_set(S) orelse error(function_clause, [F, S]),
+    sets:fold(fun(E, Acc) -> sets:add_element(F(E), Acc) end, sets:new(), S).
+-endif.
+
+-ifndef(HAVE_sets__filtermap_2).
+filtermap(F, S) when is_function(F, 1) ->
+    sets:is_set(S) orelse error(function_clause, [F, S]),
+    sets:fold(fun(E, A) ->
+                  case F(E) of
+                      true -> sets:add_element(E, A);
+                      {true, E1} -> sets:add_element(E1, A);
+                      false -> A
+                  end
+              end, sets:new(), S).
 -endif.
