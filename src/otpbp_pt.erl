f047ff3d6481349b6d3db4e51a1a6d815972fb99
Led <ledest@gmail.com>
2023-07-15 22:36:18 +0300

    otpbp_gen: remove name/1, unregister_name/1, get_proc_name/1, get_parent/0, debug_options/2

--- src/otpbp_pt.erl
+++ src/otpbp_pt.erl
@@ -118,8 +118,6 @@
                               {{gb_sets, foreach, 2}, otp_gb_sets},
                               {{gb_trees, [take, take_any], 2}, otpbp_gb_trees},
                               {{gb_trees, foreach, 2}, otpbp_gb_trees},
-                              {{gen, get_parent, 0}, otpbp_gen},
-                              {{gen, [debug_options, get_proc_name, name, unregister_name], 1}, otpbp_gen},
                               {{gen, debug_options, 2}, otpbp_gen},
                               {{gen_event, [start, start_link], 2}, otpbp_gen_event},
                               {{gen_event, start_monitor, [0, 1, 2]}, otpbp_gen_event},
