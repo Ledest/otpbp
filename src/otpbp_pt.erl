70f44b91a23d7541a46ff9339b250376dfef71c5
Led <ledest@gmail.com>
2023-08-01 01:31:45 +0300

    Add otpbp_gen_statem module
    - add enter_loop/4

--- src/otpbp_pt.erl
+++ src/otpbp_pt.erl
@@ -121,6 +121,7 @@
                               {{gen, debug_options, 2}, otpbp_gen},
                               {{gen_event, [start, start_link], 2}, otpbp_gen_event},
                               {{gen_event, start_monitor, [0, 1, 2]}, otpbp_gen_event},
+                              {{gen_statem, enter_loop, 4}, otpbp_gen_statem},
                               {{inet, [info, ipv4_mapped_ipv6_address], 1}, otpbp_inet},
                               {{io_lib, format, 3}, otpbp_io_lib},
                               {{io_lib, fwrite, 3}, {otpbp_io_lib, format}},
