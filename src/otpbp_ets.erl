753a31b706baf382b3b05fb740c5d1d5197dcdb2
Led <ledest@gmail.com>
2023-07-31 21:37:05 +0300

    otpbp_ets: optimize lookup_element/4

--- src/otpbp_ets.erl
+++ src/otpbp_ets.erl
@@ -22,13 +22,12 @@ whereis(T) ->
 lookup_element(Table, Key, Pos, Default) ->
     case ets:lookup(Table, Key) of
         [Object|_] = Objects ->
+            F = fun(O) when tuple_size(O) >= Pos -> element(Pos, O);
+                   (_) -> error(badarg, [Table, Key, Pos, Default])
+                end,
             case ets:info(Table, type) of
-                T when T =:= bag; T =:= duplicate_bag ->
-                    lists:map(fun(O) when tuple_size(O) >= Pos -> element(Pos, O);
-                                 (_) -> error(badarg, [Table, Key, Pos, Default])
-                              end, Objects);
-                _ when tuple_size(Object) >= Pos -> element(Pos, Object);
-                _ -> error(badarg, [Table, Key, Pos, Default])
+                T when T =:= bag; T =:= duplicate_bag -> lists:map(F, Objects);
+                _ -> F(Object)
             end;
         _ -> Default
     end.
