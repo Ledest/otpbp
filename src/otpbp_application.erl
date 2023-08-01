753cfbf97937dcbe02c31341ef6ca5a674971486
Led <ledest@gmail.com>
2023-08-01 19:21:42 +0300

    otpbp_application: shrink

--- src/otpbp_application.erl
+++ src/otpbp_application.erl
@@ -1,27 +1,18 @@
 -module(otpbp_application).
 
--ifndef(HAVE_application__set_env_2).
 -ifdef(HAVE_application_controller__set_env_2).
+-ifndef(HAVE_application__set_env_2).
 % OTP 21.3
 -export([set_env/2]).
--ifndef(NEED_application__set_env_2).
--define(NEED_application__set_env_2, true).
 -endif.
--endif.
+-ifndef(HAVE_application__set_env_1).
+% OTP 21.3
+-export([set_env/1]).
 -endif.
 
 -ifndef(HAVE_application__set_env_1).
 -ifdef(HAVE_application__set_env_2).
-% OTP 21.3
--export([set_env/1]).
 -import(application, [set_env/2]).
--else.
--ifdef(HAVE_application_controller__set_env_2).
-% OTP 21.3
--export([set_env/1]).
--ifndef(NEED_application__set_env_2).
--define(NEED_application__set_env_2, true).
--endif.
 -endif.
 -endif.
 -endif.
@@ -31,7 +22,8 @@
 -export([get_supervisor/1]).
 -endif.
 
--ifdef(NEED_application__set_env_2).
+-ifdef(HAVE_application_controller__set_env_2).
+-ifndef(HAVE_application__set_env_2).
 set_env(Config, Opts) when is_list(Config), is_list(Opts) ->
     case application_controller:set_env(Config, Opts) of
         ok -> ok;
@@ -40,7 +32,6 @@ set_env(Config, Opts) when is_list(Config), is_list(Opts) ->
 -endif.
 
 -ifndef(HAVE_application__set_env_1).
--ifdef(HAVE_application_controller__set_env_2).
 set_env(Config) when is_list(Config) -> set_env(Config, []).
 -endif.
 -endif.
