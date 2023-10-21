6306ddde2a1080a0ba079b16785f8eb22c84e624
Led <ledest@gmail.com>
2023-10-22 00:16:49 +0300

    otpbp_c: add ht/1,2,3

--- src/otpbp_c.erl
+++ src/otpbp_c.erl
@@ -38,6 +38,18 @@
 % OTP 23.0
 -export([hcb/3]).
 -endif.
+-ifndef(HAVE_c__ht_1).
+% OTP 23.0
+-export([ht/1]).
+-endif.
+-ifndef(HAVE_c__ht_2).
+% OTP 23.0
+-export([ht/2]).
+-endif.
+-ifndef(HAVE_c__ht_3).
+% OTP 23.0
+-export([ht/3]).
+-endif.
 
 -ifndef(HAVE_c__lm_0).
 lm() -> lists:map(fun c:l/1, c:mm()).
@@ -227,4 +239,16 @@ hcb(M, F) when is_atom(M), is_atom(F) -> {error, missing}.
 
 -ifndef(HAVE_c__hcb_3).
 hcb(M, F, A) when is_atom(M), is_atom(F), is_integer(A) -> {error, missing}.
+-endif.
+
+-ifndef(HAVE_c__ht_1).
+ht(M) when is_atom(M) -> {error, missing}.
+-endif.
+
+-ifndef(HAVE_c__ht_2).
+ht(M, F) when is_atom(M), is_atom(F) -> {error, missing}.
+-endif.
+
+-ifndef(HAVE_c__ht_3).
+ht(M, F, A) when is_atom(M), is_atom(F), is_integer(A) -> {error, missing}.
 -endif.
