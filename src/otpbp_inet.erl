99226bc34eaf9f470ce1505bab434dc5d4a151cb
Led <ledest@gmail.com>
2023-10-26 08:00:40 +0300

    otpbp_inet: add is_ip_address/1, is_ipv4_address/1, is_ipv6_address/1

--- src/otpbp_inet.erl
+++ src/otpbp_inet.erl
@@ -13,6 +13,27 @@
 % OTP 24.0
 -export([info/1]).
 -endif.
+-ifndef(HAVE_inet__is_ip_address_1).
+% OTP 25.0
+-export([is_ip_address/1]).
+-endif.
+-ifndef(HAVE_inet__is_ipv4_address_1).
+% OTP 25.0
+-export([is_ipv4_address/1]).
+-endif.
+-ifndef(HAVE_inet__is_ipv6_address_1).
+% OTP 25.0
+-export([is_ipv6_address/1]).
+-endif.
+
+-ifndef(HAVE_inet__is_ip_address_1).
+-ifdef(HAVE_inet__is_ipv4_address_1).
+-import(inet, [is_ipv4_address/1]).
+-endif.
+-ifdef(HAVE_inet__is_ipv6_address_1).
+-import(inet, [is_ipv6_address/1]).
+-endif.
+-endif.
 
 -ifndef(HAVE_inet__ipv4_mapped_ipv6_address_1).
 ipv4_mapped_ipv6_address({D1, D2, D3, D4}) when (D1 bor D2 bor D3 bor D4) =< 16#FF ->
@@ -53,4 +74,22 @@ port_info(PI, Items) -> port_info(PI, Items, []).
 
 port_info(_PI, [], Acc) -> Acc;
 port_info(PI, [Item | Items], Acc) -> port_info(PI, Items, [{Item, proplists:get_value(Item, PI)}|Acc]).
+-endif.
+
+-ifndef(HAVE_inet__is_ip_address_1).
+is_ip_address(Address) -> is_ipv4_address(Address) orelse is_ipv6_address(Address).
+-endif.
+
+-ifndef(HAVE_inet__is_ipv4_address_1).
+-define(ip(A, B, C, D), (A bor B bor C bor D) band (bnot 16#ff) =:= 0).
+
+is_ipv4_address({A, B, C, D}) when ?ip(A, B, C, D) -> true;
+is_ipv4_address(_) -> false.
+-endif.
+
+-ifndef(HAVE_inet__is_ipv6_address_1).
+-define(ip6(A, B, C, D, E, F, G, H), (A bor B bor C bor D bor E bor F bor G bor H) band (bnot 16#ffff) =:= 0).
+
+is_ipv6_address({A, B, C, D, E, F, G, H}) when ?ip6(A, B, C, D, E, F, G, H) -> true;
+is_ipv6_address(_) -> false.
 -endif.
