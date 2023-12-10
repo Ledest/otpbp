ab1e3ac63b1f87f8ef2816d218425bfc0d31963c
Led <ledest@gmail.com>
2023-12-10 17:44:10 +0200

    otpbp_ordsets: shrink foreach/2

--- src/otpbp_ordsets.erl
+++ src/otpbp_ordsets.erl
@@ -37,7 +37,5 @@ is_equal(S1, S2) when is_list(S1), is_list(S2) -> S1 == S2.
 -endif.
 
 -ifndef(HAVE_ordsets__foreach_2).
-foreach(F, S) when is_function(F, 1) ->
-    ordsets:is_set(S) orelse error(function_clause, [F, S]),
-    lists:foreach(F, ordsets:to_list(S)).
+foreach(F, S) when is_function(F, 1) -> lists:foreach(F, ordsets:to_list(S)).
 -endif.
