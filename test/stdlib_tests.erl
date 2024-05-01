e0c86e6028695f2e2201a872d3b9f4aa46679fcd
Led <ledest@gmail.com>
2024-05-01 10:03:04 +0300

    stdlib_tests: remove obsoleted tests

--- test/stdlib_tests.erl
+++ test/stdlib_tests.erl
@@ -6,16 +6,12 @@
 
 dict_test() ->
     D = dict:from_list([{a, 1}]),
-    ?assert(dict:is_empty(dict:new())),
-    ?assertNot(dict:is_empty(D)),
     ?assertEqual(dict:take(a, D), {1, dict:new()}),
     ?assertEqual(dict:take(a, dict:new()), error),
     ?assertEqual(dict:take(b, D), error).
 
 orddict_test() ->
     D = orddict:from_list([{3, c}, {1, a}, {2, b}]),
-    ?assert(orddict:is_empty(orddict:new())),
-    ?assertNot(orddict:is_empty(D)),
     ?assertEqual(orddict:take(1, D), {a, orddict:from_list([{3, c}, {2, b}])}),
     ?assertEqual(orddict:take(1, orddict:new()), error),
     ?assertEqual(orddict:take(4, D), error).
@@ -29,17 +25,11 @@ ordsets_test() ->
     ?assertNot(ordsets:is_empty(ordsets:from_list([3,1,2]))).
 
 gb_trees_test() ->
-    D1 = gb_trees:from_orddict(orddict:from_list([{3, c}, {1, a}, {2, b}])),
-    ?assertEqual({1, a, gb_trees:iterator_from(2, D1)}, gb_trees:next(gb_trees:iterator(D1))),
-    D2 = gb_trees:insert(a, 1, gb_trees:empty()),
-    ?assertEqual(gb_trees:take(a, D2), {1, gb_trees:empty()}),
-    ?assertEqual(gb_trees:take_any(a, D2), {1, gb_trees:empty()}),
+    D = gb_trees:from_orddict(orddict:from_list([{a, 1}])),
+    ?assertEqual(gb_trees:take(a, D), {1, gb_trees:empty()}),
+    ?assertEqual(gb_trees:take_any(a, D), {1, gb_trees:empty()}),
     ?assertEqual(gb_trees:take_any(a, gb_trees:empty()), error),
-    ?assertEqual(gb_trees:take_any(b, D2), error).
-
-gb_sets_test() ->
-    S = gb_sets:from_ordset(ordsets:from_list([3, 1, 2])),
-    ?assertEqual({1, gb_sets:iterator_from(2, S)}, gb_sets:next(gb_sets:iterator(S))).
+    ?assertEqual(gb_trees:take_any(b, D), error).
 
 lists_test() ->
     % search/2
