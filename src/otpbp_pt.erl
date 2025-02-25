4d02299bc073ba9a6eba4caa913f46773b382476
Led <ledest@gmail.com>
2025-02-25 14:44:45 +0200

    otpbp_calendar: add local_time_to_system_time/{1,2}, universal_time_to_system_time/{1,2}

--- src/otpbp_pt.erl
+++ src/otpbp_pt.erl
@@ -38,6 +38,7 @@
          {{c, erlangrc, 1}, otpbp_c},
          {{c, [h, hcb, ht], [1, 2, 3]}, otpbp_c},
          {{c, mm, 0}, {otpbp_code, modified_modules}},
+         {{calendar, [local_time_to_system_time, universal_time_to_system_time], [1, 2]}, otpbp_calendar},
          {{calendar, [rfc3339_to_system_time, system_time_to_rfc3339], [1, 2]}, otpbp_calendar},
          {{calendar, [system_time_to_local_time, system_time_to_universal_time], 2}, otpbp_calendar},
          {{code, [all_available, clear_cache, module_status, rehash], 0}, otpbp_code},
