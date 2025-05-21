c8fef9d3cc50c9f29218f208d498b840896c4263
Led <ledest@gmail.com>
2025-05-21 17:38:16 +0300

    otpbp_erlang: add processes_iterator/0, processes_next/1

--- src/otpbp_pt.erl
+++ src/otpbp_pt.erl
@@ -114,6 +114,8 @@
          {{erlang, get_stacktrace, 0}, otpbp_erlang},
          {{erlang, [get_cookie, set_cookie], 1}, otpbp_erlang},
          {{erlang, iolist_to_iovec, 1}, otpbp_erlang},
+         {{erlang, processes_iterator, 0}, otpbp_erlang},
+         {{erlang, processes_next, 1}, otpbp_erlang},
          {{erpc, reqids_new, 0}, {maps, new}},
          {{erpc, [reqids_size, reqids_to_list], 1}, otpbp_erpc},
          {{erpc, [check_response, receive_response, reqids_add, wait_response], 3}, otpbp_erpc},
