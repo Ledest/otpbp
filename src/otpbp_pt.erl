b6bcf58bf8d83f0bdcb16eca8f1ef41421ebb1ac
Led <ledest@gmail.com>
2025-10-03 02:12:47 +0300

    otpbp_pt: add supervisor:stop/1 and supervisor:stop/3 support

--- src/otpbp_pt.erl
+++ src/otpbp_pt.erl
@@ -253,6 +253,7 @@
          {{ssl, prf, 5}, otpbp_ssl},
          {{string, jaro_similarity, 2}, otpbp_string},
          {{supervisor, check_childspecs, 2}, otpbp_supervisor},
+         {{supervisor, stop, [1, 3]}, gen_server},
          {{sys, get_log, 1}, otpbp_sys},
          {{timer, [apply_after, apply_interval], [2, 3]}, otpbp_timer},
          {{timer, tc, 4}, otpbp_timer},
