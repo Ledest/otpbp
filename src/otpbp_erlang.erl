5fcd9bd4ec95aa7b5cb7cb819f23609aaf7bbb0e
Led <ledest@gmail.com>
2026-03-31 02:12:56 +0300

    otpbp_erlang: add exit_signal/2, exit_signal/3, exit/3

--- src/otpbp_erlang.erl
+++ src/otpbp_erlang.erl
@@ -60,6 +60,14 @@
 % OTP 29.0
 -export([is_integer/3]).
 -endif.
+-ifndef(HAVE_erlang__exit_3).
+% OTP 29.0
+-export([exit/3]).
+-endif.
+-ifndef(HAVE_erlang__exit_signal_3).
+% OTP 29.0
+-export([exit_signal/3]).
+-endif.
 
 -ifndef(HAVE_erlang__ceil_1).
 -ifdef(HAVE_math__ceil_1).
@@ -125,6 +133,18 @@ processes_iterator() -> {0, processes()}.
 is_integer(T, L, U) ->
     is_integer(L) andalso is_integer(U) orelse error(badarg, [T, L, U]),
     is_integer(T) andalso T >= L andalso T =< U.
+-endif.
+
+-ifndef(HAVE_erlang__exit_3).
+exit(Dest, Reason, OptList) when OptList =:= []; OptList =:= [priority] -> exit(Dest, Reason).
+-endif.
+
+-ifndef(HAVE_erlang__exit_signal_3).
+-ifdef(HAVE_erlang__exit_signal_3).
+exit_signal(Dest, Reason, OptList) -> exit(Dest, Reason, OptList).
+-else.
+exit_signal(Dest, Reason, OptList) when OptList =:= []; OptList =:= [priority] -> exit(Dest, Reason).
+-endif.
 -endif.
 
 -ifndef(HAVE_erlang__processes_next_1).
